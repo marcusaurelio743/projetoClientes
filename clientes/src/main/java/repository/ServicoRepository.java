5b855ac052b0ad1291546fd2fac39ef663426836
Home <Home@DESKTOP-F8NUUGM>
2023-04-21 19:20:36 -0300

    criando as interfaces repository de cliente e Serviço

---
+++ clientes/src/main/java/repository/ServicoRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+
+}
