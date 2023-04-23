7500b03be7b45f720338fdea587d9677667f2d6e
Home <Home@DESKTOP-F8NUUGM>
2023-04-23 16:36:27 -0300

    criando o clienteController e o metodo de salvar

---
+++ clientes/src/main/java/ProjetoClientes/rest/clienteController.java
@@ -0,0 +1,32 @@
+package ProjetoClientes.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import ProjetoClientes.model.entity.Cliente;
+import ProjetoClientes.model.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class clienteController {
+	
+	private ClienteRepository repository;
+	
+	//injeção de dependencia do cliente repository para classe controller
+	
+	@Autowired
+	public clienteController(ClienteRepository repository) {
+		this.repository = repository;
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente salvar(Cliente cliente) {
+		return repository.save(cliente);
+	}
+
+}
