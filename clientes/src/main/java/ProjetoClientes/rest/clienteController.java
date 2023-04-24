4a6bcdd00fabcb5f96ab3ac3bd7e4e531e133d0f
Home <Home@DESKTOP-F8NUUGM>
2023-04-24 20:37:33 -0300

    criando o metodo buscar cliente pelo id e usando a anotação getmapping e retornando o objeto ou erro caso não encontre

--- clientes/src/main/java/ProjetoClientes/rest/clienteController.java
+++ clientes/src/main/java/ProjetoClientes/rest/clienteController.java
@@ -2,11 +2,14 @@ package ProjetoClientes.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import ProjetoClientes.model.entity.Cliente;
 import ProjetoClientes.model.repository.ClienteRepository;
@@ -28,6 +31,14 @@ public class clienteController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente salvar(@RequestBody Cliente cliente) {
 		return repository.save(cliente);
+	}
+	
+	@GetMapping("{codigo}")
+	public Cliente buscaPorId(@PathVariable("codigo") Long id) {
+		return repository
+				.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		
 	}
 
 }
