6b54efc856dd1b7496f43e147c9477535f059d5b
Home <Home@DESKTOP-F8NUUGM>
2023-04-24 21:32:08 -0300

    criando o metodo deletar e usando @deletemapping e tratando erro

--- clientes/src/main/java/ProjetoClientes/rest/clienteController.java
+++ clientes/src/main/java/ProjetoClientes/rest/clienteController.java
@@ -2,6 +2,7 @@ package ProjetoClientes.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,19 @@ public class clienteController {
 				.findById(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 		
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long id) {
+		repository
+		.findById(id)
+		.map(cliente->{
+			repository.delete(cliente);
+			return Void.TYPE;
+		})
+		.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
 	}
 
 }
