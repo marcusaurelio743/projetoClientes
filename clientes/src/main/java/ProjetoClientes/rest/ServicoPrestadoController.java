09ca523838a3a78b312804f144cf770691e39870
Home <Home@DESKTOP-F8NUUGM>
2023-05-28 17:41:52 -0300

    atualizando projeto

--- clientes/src/main/java/ProjetoClientes/rest/ServicoPrestadoController.java
+++ clientes/src/main/java/ProjetoClientes/rest/ServicoPrestadoController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,7 @@ import ProjetoClientes.util.BigDecimalConverter;
 
 @RestController
 @RequestMapping("api/servicos-prestados")
+@CrossOrigin("http://localhost:4200")
 public class ServicoPrestadoController {
 	
 	private final ClienteRepository clienteRepository;
