4e81a875154b4e1fa973db4cb9d05e93605ec5db
Home <Home@DESKTOP-F8NUUGM>
2023-06-29 16:50:28 -0300

    configurando authorizationServerConfig

--- clientes/src/main/java/ProjetoClientes/config/AuthorizationServerConfig.java
+++ clientes/src/main/java/ProjetoClientes/config/AuthorizationServerConfig.java
@@ -1,5 +1,48 @@
 package ProjetoClientes.config;
 
-public class AuthorizationServerConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+		.tokenStore(tokenStore())
+		.authenticationManager(authenticationManager)
+		
+		;
+	}
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients
+			.inMemory()
+			.withClient("my-angular-app")
+			.secret("@321")
+			.scopes("read","write")
+			.authorizedGrantTypes("password")
+			//tempo de duração do tokem nesse exemplo é meia hora
+			.accessTokenValiditySeconds(1800)
+		;
+	}
 
 }
